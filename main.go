b623d17c1a7cce46b241493c369d400be8a2ae2c
Yunjin Lee <gzblues61@gmail.com>
2025-04-05 02:41:43 +0900

    edited main.go.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
     client "github.com/lxc/incus/client"
     http_request "github.com/yoonjin67/linux_virt_unit/http_request"
-    linux_virt "github.com/yoonjin67/linux_virt_unit"
     db "github.com/yoonjin67/linux_virt_unit/mongo_connect"
     incus_unit "github.com/yoonjin67/linux_virt_unit/incus_unit"
     "log"
@@ -11,19 +10,18 @@ import (
 )
 
 // 전역 변수 선언
-var WorkQueue chan int
 
 func main() {
     // PortHeap 초기화
+    incus_unit.InitWorkQueue()
     incus_unit.PortHeap = &incus_unit.IntHeap{}
-    incus_unit.WorkQueue = &incus_unit.ContainerQueue{
-        Tasks: make(chan linux_virt.ContainerInfo, 100),
-    }
     heap.Init(incus_unit.PortHeap)
 
     log.Println("Port heap allocation succeed.")
 
     var err error
+    incus_unit.WorkQueue.Start(5) // 5개의 작업자 시작
+    defer incus_unit.WorkQueue.Stop()
     db.InitMongoDB()
     defer db.CloseMongoDB()
     incus_unit.IncusCli, err = client.ConnectIncusUnix("", nil)
@@ -34,7 +32,7 @@ func main() {
     // WorkQueue 초기화
 
     // HTTP 요청 초기화
-    http_request.InitHttpRequest(incus_unit.WorkQueue)
+    http_request.InitHttpRequest()
 
     // 컨테이너 작업자 풀 시작
 }
