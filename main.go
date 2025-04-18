93cc922b80a73e2644a003557838f19026835cd2
Yunjin Lee <gzblues61@gmail.com>
2025-04-18 21:50:00 +0900

    applied go fmt formatterc

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-        client "github.com/lxc/incus/client"
-        http_request "github.com/yoonjin67/linux_virt_unit/http_request"
-        db "github.com/yoonjin67/linux_virt_unit/mongo_connect"
-        incus_unit "github.com/yoonjin67/linux_virt_unit/incus_unit"
-        "log"
-
+	client "github.com/lxc/incus/client"
+	http_request "github.com/yoonjin67/linux_virt_unit/http_request"
+	incus_unit "github.com/yoonjin67/linux_virt_unit/incus_unit"
+	db "github.com/yoonjin67/linux_virt_unit/mongo_connect"
+	"log"
 )
 
 // @title Linux Virtualization API
@@ -16,17 +15,17 @@ import (
 // @BasePath /
 
 func main() {
-    incus_unit.InitWorkQueue()
-    var err error
-    incus_unit.WorkQueue.Start(24)
-    defer incus_unit.WorkQueue.Stop()
-    db.InitMongoDB()
-    defer db.CloseMongoDB()
-    incus_unit.IncusCli, err = client.ConnectIncusUnix("", nil)
-    if err != nil {
-            log.Fatalf("Failed to connect to Incus: %v", err)
-    }
+	incus_unit.InitWorkQueue()
+	var err error
+	incus_unit.WorkQueue.Start(24)
+	defer incus_unit.WorkQueue.Stop()
+	db.InitMongoDB()
+	defer db.CloseMongoDB()
+	incus_unit.IncusCli, err = client.ConnectIncusUnix("", nil)
+	if err != nil {
+		log.Fatalf("Failed to connect to Incus: %v", err)
+	}
 
-    http_request.InitHttpRequest()
+	http_request.InitHttpRequest()
 
 }
